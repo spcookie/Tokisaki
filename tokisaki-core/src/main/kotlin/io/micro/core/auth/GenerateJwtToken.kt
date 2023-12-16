0e06b12262b1a6bc9aea7e2e70ecf0ee166f2163
cokie <liuweixiang@come-future.com>
2023-12-16 15:27:22 +0800

    fix: 优化Token

--- tokisaki-core/src/main/kotlin/io/micro/core/auth/GenerateJwtToken.kt
+++ tokisaki-core/src/main/kotlin/io/micro/core/auth/GenerateJwtToken.kt
@@ -2,6 +2,7 @@ package io.micro.core.auth
 
 import io.smallrye.jwt.build.Jwt
 import org.eclipse.microprofile.jwt.Claims
+import java.time.Duration
 
 
 /**
@@ -9,14 +10,21 @@ import org.eclipse.microprofile.jwt.Claims
  *@since 2023/11/25
  */
 object GenerateJwtToken {
+
+    private val expires = Duration.ofDays(7)
+
+    private val issuer = "Tokisaki"
+
     /**
      * 生成JWT令牌
      */
     fun generate(name: String, openId: String, authorities: Set<String>): String {
-        return Jwt.issuer("https://example.com/issuer")
-            .upn(name)
+        return Jwt.issuer(issuer)
+            .upn(openId)
             .groups(authorities)
-            .claim(Claims.orig.name, openId)
+            .claim(Claims.nickname.name, name)
+            .expiresIn(expires)
             .sign()
     }
+
 }
