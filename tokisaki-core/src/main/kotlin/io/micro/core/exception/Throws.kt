26f27191161481fabc2699a3c90f26ba735b08ea
cokie <liuweixiang@come-future.com>
2023-12-23 17:49:46 +0800

    :bug: fix: 修改异常断言

--- tokisaki-core/src/main/kotlin/io/micro/core/exception/Throws.kt
+++ tokisaki-core/src/main/kotlin/io/micro/core/exception/Throws.kt
@@ -1,34 +1,78 @@
 package io.micro.core.exception
 
 import java.util.*
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
 
+@OptIn(ExperimentalContracts::class)
 object Throws {
 
     fun fail(msg: String): Nothing {
         throw BusinessException(msg)
     }
 
-    fun failIfTure(block: () -> Boolean, msg: String) {
+    fun requireTure(msg: String, block: () -> Boolean) {
+        contract {
+            callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+        }
         if (block()) {
             fail(msg)
         }
     }
 
-    fun failIfFalse(block: () -> Boolean, msg: String) {
+    fun requireTure(block: Boolean, msg: String) {
+        if (block) {
+            fail(msg)
+        }
+    }
+
+    fun requireFalse(msg: String, block: () -> Boolean) {
+        contract {
+            callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+        }
         if (!block()) {
             fail(msg)
         }
     }
 
-    fun <T> failIfNull(block: () -> T, msg: String) {
+    fun requireFalse(value: Boolean, msg: String) {
+        if (!value) {
+            fail(msg)
+        }
+    }
+
+    fun <T> requireNull(msg: String, block: () -> T) {
+        contract {
+            callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+        }
         if (Objects.isNull(block())) {
             fail(msg)
         }
     }
 
-    fun <T> failIfNonNull(block: () -> T, msg: String) {
+    fun <T> requireNull(value: T, msg: String) {
+        if (Objects.isNull(value)) {
+            fail(msg)
+        }
+    }
+
+    fun <T> requireNonNull(msg: String, block: () -> T) {
+        contract {
+            callsInPlace(block, InvocationKind.EXACTLY_ONCE)
+        }
         if (!Objects.isNull(block())) {
             fail(msg)
         }
     }
+
+    fun <T> requireNonNull(value: T, msg: String) {
+        contract {
+            returns() implies (value != null)
+        }
+        if (!Objects.isNull(value)) {
+            fail(msg)
+        }
+    }
+
 }
