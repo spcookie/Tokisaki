986927f16be6fd0e210ddc5447246651721a98a1
cokie <liuweixiang@come-future.com>
2023-12-23 18:36:50 +0800

    :arrow_up: fix: 升级QQ机器人依赖

--- tokisaki-core/build.gradle.kts
+++ tokisaki-core/build.gradle.kts
@@ -5,6 +5,6 @@ plugins {
 
 dependencies {
     api("cn.hutool:hutool-core:5.8.16")
-    implementation("net.mamoe:mirai-core:2.15.0")
-    implementation("net.mamoe:mirai-logging-slf4j:2.15.0")
+    implementation("net.mamoe:mirai-core:2.16.0")
+    implementation("net.mamoe:mirai-logging-slf4j:2.16.0")
 }
