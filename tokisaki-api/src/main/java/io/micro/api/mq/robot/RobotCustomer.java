2dcb5c5cddc82ad4c7d77519444fc156d3b5800b
cokie <liuweixiang@come-future.com>
2024-01-22 19:45:50 +0800

    :fire: feat: 添加mq

--- tokisaki-api/src/main/java/io/micro/api/mq/robot/RobotCustomer.java
+++ tokisaki-api/src/main/java/io/micro/api/mq/robot/RobotCustomer.java
@@ -2,6 +2,7 @@ package io.micro.api.mq.robot;
 
 import io.micro.server.robot.infra.event.dto.RobotDTO;
 import io.micro.server.robot.infra.event.sdk.RobotConstant;
+import io.vertx.core.json.JsonObject;
 import jakarta.enterprise.context.ApplicationScoped;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Message;
@@ -12,8 +13,9 @@ import java.util.concurrent.CompletionStage;
 public class RobotCustomer {
 
     @Incoming(RobotConstant.Queue.ROBOT_LOGIN_SUCCESS)
-    public CompletionStage<Void> robotLoginSuccess(Message<RobotDTO> msg) {
-        RobotDTO robot = msg.getPayload();
+    public CompletionStage<Void> robotLoginSuccess(Message<JsonObject> msg) {
+        JsonObject jsonObject = msg.getPayload();
+        RobotDTO robot = jsonObject.mapTo(RobotDTO.class);
         System.out.println("==========");
         System.out.println(robot.getId());
         System.out.println("==========");
